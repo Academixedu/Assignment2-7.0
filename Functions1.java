4eedc119a46bd5252131cc65ee3c38c25595a188
preetham0223 <sagilinoyal@gmail.com>
2024-07-16 14:19:27 +0530

    commit change

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,13 @@
 public class Functions1{
-// Create One Function/Method to get emp data
-  // method paramaters should contain eid,ename,esal,edesg and return type should be void
-  // Write a Print Statement in method and Print it
   public static void main(String[]args){
-  Functions1 f=new Functions1();
+    Functions1 f=new Functions1();
+    f.emp(249,"preetham" ,2000,"associate");
   }
-}
+  public static void emp(int eid,String ename,int esal, String edesg){
+    System.out.println("emp id is " +eid);
+    System.out.println("emp name is " +ename);
+    System.out.println("emp salary is " +esal);
+    System.out.println("emp desg is " +edesg);
+  }
+  }
+ 
