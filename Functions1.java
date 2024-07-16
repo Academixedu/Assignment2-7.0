3e5ccc952777df473205ac0e0502c66e4f93fd85
psvsai <pasupuletisvsm245@gmail.com>
2024-07-16 18:01:56 +0530

    changes

--- Functions1.java
+++ Functions1.java
@@ -2,7 +2,16 @@ public class Functions1{
 // Create One Function/Method to get emp data
   // method paramaters should contain eid,ename,esal,edesg and return type should be void
   // Write a Print Statement in method and Print it
-  public static void main(String[]args){
-  Functions1 f=new Functions1();
+  public static void main (String[]args) {
+    Functions1 f=new Functions1();
+    f.employee(20987, "PSV SAI MAHESH", 2000, "ASSOCIATE");
+
+  }
+  public static void  employee(int Employeeid, String  EmployeeName, int Employeesalary,String designation) {
+    System.out.println("Employee ID is " + Employeeid);
+    System.out.println("Employee Name is " + EmployeeName);
+    System.out.println("Employee salary is " + Employeesalary);
+    System.out.println("Employee designation is " + designation);
+
   }
 }
--- PrintName.java
+++ PrintName.java
@@ -2,5 +2,14 @@ public class PrintName {
   // Prepare Function with Name Parameter to get your name
 public static void main(String[] args) {
   // Complete the Logic
+  String result = Name( "PSV SAI MAHESH");
+  System.out.println("I am " +  result);
+  
+}
+public static String Name(String n) {
+ return n;
+
+  
 }
 }
+
