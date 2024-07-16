0afbf7684fc0593a6bc16dcdd92a68e9f5713c9e
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-16 15:10:58 +0530

    assign

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,24 @@
+
+
 public class Functions1{
 // Create One Function/Method to get emp data
   // method paramaters should contain eid,ename,esal,edesg and return type should be void
   // Write a Print Statement in method and Print it
   public static void main(String[]args){
   Functions1 f=new Functions1();
+  f.Employeedata(12345,"Swarupa",50000.0,"Software");
   }
-}
+  public static void Employeedata(int Id,String name,Double Salary,String desg){
+    System.out.println("ID is: " + Id);
+    System.out.println("Name is: " + name);
+    System.out.println("Salary is: " +Salary);
+    System.out.println("Designation is: " +desg);
+    /*return number1;*/
+  }
+  /*public static String name(String name){
+    return name;*/
+
+
+  }
+
+
