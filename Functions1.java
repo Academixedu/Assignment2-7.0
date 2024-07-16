0dd2582f3c5a89d92845db0a9afb63c876f18554
ratan-17 <rkmule2002@gmail.com>
2024-07-16 16:11:58 +0530

    msg

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,17 @@
+
+
 public class Functions1{
-// Create One Function/Method to get emp data
-  // method paramaters should contain eid,ename,esal,edesg and return type should be void
-  // Write a Print Statement in method and Print it
-  public static void main(String[]args){
+    public void empData(int eid, String ename, int esal, String edesg)
+    {
+      System.out.println("Employee id is "+ eid);
+      System.out.println("Employee Name is "+ ename);
+      System.out.println("Employee Salary is "+ esal);
+      System.out.println("Employee Designation is "+ edesg);
+    }
+  
+  public static void main(String[]args)
+  {
   Functions1 f=new Functions1();
+    f.empData(25, "Ratan", 80000, "Senior Java Developer");
   }
 }
