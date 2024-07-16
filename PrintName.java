ed53352c39b4f7ba4b93da0dd5a963a88ae27101
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-16 13:50:46 +0530

    syam

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,20 @@
 public class PrintName {
+
+  public static String Name(String name1){
+    return name1;
+  }
+  public static void main(String[] args) {
+    String res=Name("syam");
+    System.out.println("my name is:"+res);
+  }
+  // public static void Name(String name1){
+  //   System.out.println("my name is:"+name1);
+  // }
+  // public static void main(String[] args) {
+  //   Name("syam");
+  }
+
+
+  
   // Prepare Function with Name Parameter to get your name
-public static void main(String[] args) {
-  // Complete the Logic
-}
-}
+
