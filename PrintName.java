7d21f11892fa1dd07cc70f5c91b00f3d517beb4d
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-16 14:02:15 +0530

    print name

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,13 @@
 public class PrintName {
   // Prepare Function with Name Parameter to get your name
 public static void main(String[] args) {
+
   // Complete the Logic
+  System.out.println("my name is"+name("pavani"));
+}
+public static String name(String a){
+
+
+return a;
 }
 }
