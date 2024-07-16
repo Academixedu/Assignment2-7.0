fd64c37f650ccd5bbc68ae0d8eeab262acecea1b
sujatha-94 <sujathakarri701@gmail.com>
2024-07-16 15:09:17 +0530

    helloo

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,10 @@
 public class PrintName {
   // Prepare Function with Name Parameter to get your name
+  public static void name(String Sujatha){
+    System.out.println("name:" +Sujatha);
+  }
 public static void main(String[] args) {
   // Complete the Logic
+  name("Sujatha");
 }
 }
--- SubtractNumbers.java
+++ SubtractNumbers.java
@@ -1,8 +1,13 @@
 public class SubtractNumbers {
-  // Create a Function to get value from the given numbers
+  // Create a Function to get value from the given numbers 
+  public static int subtract(int a,int b){
+    int result = a - b;
+    return result;
+  }
+
+
   public static void main(String[] args) {
         int result = subtract(7, 2);
         System.out.println("Difference: " + result);
     }
-    }
-}
+  }
