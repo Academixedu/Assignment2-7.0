22dba422df2f03985446280803917925ef761259
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-16 15:12:41 +0530

    MANI

--- Functions1.java
+++ Functions1.java
@@ -4,5 +4,16 @@ public class Functions1{
   // Write a Print Statement in method and Print it
   public static void main(String[]args){
   Functions1 f=new Functions1();
+  f.emp(07,"MANI", 100000, "Sr.developer");
+
+
+  }
+  public static void emp(int id,String ename,int esal,String edesg)
+  {
+    System.out.println("Employee id is "+id);
+    System.out.println("Employee name is "+ename);
+    System.out.println("Employee salary is "+esal);
+    System.out.println("Employee designation is "+edesg);
+
   }
 }
--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,15 @@
 public class PrintName {
   // Prepare Function with Name Parameter to get your name
 public static void main(String[] args) {
+
   // Complete the Logic
+  Function("MANI");
+  System.out.println(Function("MANI"));
+
+
+}
+public static String Function(String name)
+{
+  return name;
 }
 }
