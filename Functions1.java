a9acee5eb558916bfcd8adcc178dfe559d6d8247
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-16 19:21:07 +0530

    Update Functions1.java

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,14 @@
 public class Functions1{
-// Create One Function/Method to get emp data
-  // method paramaters should contain eid,ename,esal,edesg and return type should be void
-  // Write a Print Statement in method and Print it
+public void Employee(int eid,String ename,int esal,String edesg){
+
+  System.out.println("Employee id is "+ eid);
+  System.out.println("Employee name is "+ ename);
+  System.out.println("Employee salary is "+ esal);
+  System.out.println("Employee Designation  is "+ edesg);
+}
+
   public static void main(String[]args){
   Functions1 f=new Functions1();
+  f.Employee(7, "sarvesh", 120000, "Senior Devloper");
   }
 }
