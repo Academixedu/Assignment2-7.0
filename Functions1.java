3fc8ca38edbc6ab2ce65bc61800fbcb9f18af412
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-16 12:16:28 +0530

    Create Functions1.java

---
+++ Functions1.java
@@ -0,0 +1,8 @@
+public class Functions1{
+// Create One Function/Method to get emp data
+  // method paramaters should contain eid,ename,esal,edesg and return type should be void
+  // Write a Print Statement in method and Print it
+  public static void main(String[]args){
+  Functions1 f=new Functions1();
+  }
+}
