eaa035ccb4877a9cef9ad6cbd4aafaf60ecb44f3
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-16 14:09:01 +0530

    Name

--- PrintName.java
+++ PrintName.java
@@ -2,5 +2,12 @@ public class PrintName {
   // Prepare Function with Name Parameter to get your name
 public static void main(String[] args) {
   // Complete the Logic
+        Name("Ritesh");
+
 }
+
+        public static void Name(String name) {
+
+          System.out.println("Name :"+name);
+        }
 }
