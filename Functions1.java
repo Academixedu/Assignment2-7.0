fb46f704dd9857ee2ffece4909c743bb01c64b0d
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-16 15:22:47 +0530

    my

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,23 @@
 public class Functions1{
-// Create One Function/Method to get emp data
+
+  public void empdata(int eid,String ename,int esal,String edesg)
+  {
+    System.out.println("employee id "+eid);
+    System.out.println("employee name "+ename);
+    System.out.println("employee salary"+esal);
+    System.out.println("employee desgnation "+edesg);
+    
+  }
+// Create One Function/Method do to get emp data
   // method paramaters should contain eid,ename,esal,edesg and return type should be void
   // Write a Print Statement in method and Print it
   public static void main(String[]args){
-  Functions1 f=new Functions1();
+    
+  Functions1 f = new Functions1();
+  f.empdata(123456,"kavitha",50000,"Data Analyst");
+  
+
+  
+  
   }
 }
