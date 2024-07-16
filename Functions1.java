70b96ca5730ab43378a6fa0021a237ab4c763617
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-16 19:44:28 +0530

    Update Functions1.java

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,21 @@
-public class Functions1{
+public class Functions1
+{
 // Create One Function/Method to get emp data
   // method paramaters should contain eid,ename,esal,edesg and return type should be void
   // Write a Print Statement in method and Print it
+public void employe(int eid,String ename,int esal,String edesg )
+{
+  System.out.println("this is empolye id " + eid);
+  System.out.println("this is empolye name " + ename);
+  System.out.println("this is empolye sal " + esal);
+  System.out.println("this is empolye desg " + edesg);
+
+}
+
   public static void main(String[]args){
-  Functions1 f=new Functions1();
-  }
+
+  Functions1 asdf=new Functions1();
+  asdf.employe(21, "Abhishek", 150000, "Manager");
+ 
+}
 }
