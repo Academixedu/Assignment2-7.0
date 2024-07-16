da1fa0ebbe9e5961fd6a1e3763f2895b3963866d
sujatha-94 <sujathakarri701@gmail.com>
2024-07-16 15:17:53 +0530

    helloo

--- PrintName.java
+++ PrintName.java
@@ -1,7 +1,7 @@
 public class PrintName {
   // Prepare Function with Name Parameter to get your name
-  public static void name(String Sujatha){
-    System.out.println("name:" +Sujatha);
+  public static void name(String name){
+    System.out.println("name:" +name);
   }
 public static void main(String[] args) {
   // Complete the Logic
