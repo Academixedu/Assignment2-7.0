d3a3c8d1e7379668cfc42684f26f62cac70e7cee
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-07-16 16:41:42 +0530

    Update PrintName.java

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,7 @@
 public class PrintName {
-  // Prepare Function with Name Parameter to get your name
-public static void main(String[] args) {
-  // Complete the Logic
-}
+    
+        public static void main(String[] args) {
+            System.out.println("Shreyash Avinash Arghode");
+    }
+    
 }
