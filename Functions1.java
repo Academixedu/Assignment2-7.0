9c1b3752008e9f269205c9351b10b226405feed6
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-16 13:50:51 +0530

    change

--- Functions1.java
+++ Functions1.java
@@ -1,8 +1,18 @@
 public class Functions1{
-// Create One Function/Method to get emp data
-  // method paramaters should contain eid,ename,esal,edesg and return type should be void
-  // Write a Print Statement in method and Print it
-  public static void main(String[]args){
-  Functions1 f=new Functions1();
+  // Create One Function/Method to get emp data
+    // method paramaters should contain eid,ename,esal,edesg and return type should be void
+    // Write a Print Statement in method and Print it
+    public void empData(int eid,String ename,int esal,String edesg){
+  
+    System.out.println("employee id is :"+eid);
+    System.out.println("employee name is :"+ename);
+    System.out.println("employee salary is ::"+esal);
+    System.out.println("employee designation is :"+edesg);
+  
+    }
+    public static void main(String[]args){
+    Functions1 f=new Functions1();
+    f.empData(1, "sankeerthana", 50000,"java developer" );
+    }
   }
-}
+  
